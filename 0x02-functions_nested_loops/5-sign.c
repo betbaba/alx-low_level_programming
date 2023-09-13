aaef0e9c5d0bb1973d20aff996f4d6c44323b72c
betbaba <bettybettybaba@gmail.com>
2023-09-13 06:10:16 -0700

    ok

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -24,5 +23,4 @@ int print_sign(int n)
 	_putchar(48);
        	return (0);
 	}
-} 
-
+}
